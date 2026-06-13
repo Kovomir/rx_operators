a61648e177ba80d723ad2d998d88a12b5bd63ff8
Kovomir <honzik76kovar@gmail.com>
2026-06-13 21:24:05 +0200

    eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,19 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'react-refresh/only-export-components': [
+        'error',
+        {
+          allowConstantExport: true,
+          allowExportNames: [
+            'buttonVariants',
+            'Form',
+            'useFormField',
+            'useSidebar',
+          ],
+        },
+      ],
+    },
   },
 ])
